5bf648b42c96398544a36bb09c3eb7d7c40ea214
CadenBurks <cadendburks08@icloud.com>
2026-01-11 12:56:23 -0500

    palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(/[^\w\s]|_/g, "");
+    str = (str.replaceAll(" ", "")).toLowerCase();
+    let reversed = str.split("").reverse().join("");
+    return str === reversed;
 };
 
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;

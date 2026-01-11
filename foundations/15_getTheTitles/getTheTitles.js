03260e73b66b5b78ba88b6fd00150abde00d725b
CadenBurks <cadendburks08@icloud.com>
2026-01-11 13:49:45 -0500

    Foundations: get titles and find oldest

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    return arr.reduce((acc, obj) => {
+        acc.push(obj.title);
+        return acc;
+    }, []);
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const sorted = arr.sort((a, b) => {
+        let aAge = 0;
+        let bAge = 0;
+        if (a.yearOfDeath === undefined){
+            aAge = (new Date()).getFullYear() - a.yearOfBirth;
+        } 
+        else{
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        }
+        if (b.yearOfDeath === undefined){
+            bAge = (new Date()).getFullYear() - b.yearOfBirth;
+        } 
+        else{
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        }
+        console.log(aAge, bAge);
+        return aAge < bAge ? 1 : -1;
+    })
+    return sorted[0];
 };
 
 // Do not edit below this line
